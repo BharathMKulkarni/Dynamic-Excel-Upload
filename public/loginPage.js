f7cd08bca19dc085eb76317bdc7881a975695d4a
Lancy_Lobo <lancyjoylobo2@gmail.com>
2021-07-13 11:37:02 +0530

    login page

--- public/loginPage.js
+++ public/loginPage.js
@@ -37,6 +37,14 @@ submitBtn.onclick = (event) => {
     var textInput = document.getElementById("textField");
     phone = textInput.value;
     console.log("Inside on click", phone)
+    var elem = document.getElementById("err")
+    if(phone<1)
+    {
+        document.getElementById("err").innerHTML = `Number field is empty`; 
+        elem.style.display='block'
+    }
+    else{
+        elem.style.display='none'
     
     fetch('/login/get-otp', {
         method: 'POST',
@@ -48,10 +56,11 @@ submitBtn.onclick = (event) => {
     .then(res => res.json())
     .then(data => {
         console.log(data);
-        submitBtn.innerHTML = "Verify & Proceed";
+        submitBtn.innerHTML = "Enter OTP";
         submitBtn.onclick = verifyOtp;
         textInput.value = "";
         textInput.placeholder = "Enter the otp";
+        submitBtn.value = "Verify";
         textInput.pattern = "[0-9]{6}";
         let timer = 120; // 120 seconds time limit
         setInterval( () => {
@@ -59,11 +68,12 @@ submitBtn.onclick = (event) => {
                 $('#OTPModal').modal('show');
                 return;
             }
-            document.getElementById("timer").innerHTML = `Time Remaining: ${timer}`;
+            document.getElementById("timer").innerHTML = `${timer}`;
             timer--;
         }, 1000);
     })
     .catch(err => console.log("Error" + err));
+}
 }
 
 
