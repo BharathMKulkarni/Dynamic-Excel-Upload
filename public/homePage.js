0119e9c0f243be9321da15f5c6c62f2674bc10e2
BharathMKulkarni <bharathmkulkarni@gmail.com>
2021-07-22 08:10:58 +0530

    Fixed a bug in choose file

--- public/homePage.js
+++ public/homePage.js
@@ -22,6 +22,7 @@ const input = document.getElementById('input');
 
 input.addEventListener('click',() => {
 
+    // WHEN NO FILE IS SELECTED
     if(inputFileName === undefined){
         nameOfFileChosen.innerText = 'no file chosen';
         document.getElementById("choseFileInstruction").innerHTML = `choose a file or drag and drop a file here`;
